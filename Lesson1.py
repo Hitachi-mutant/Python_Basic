f57568d21993cf671f075b2016e8e144f2f765ac
Hitachi-mutant <goldskydiver@gmail.com>
2023-06-12 00:20:41 +0300

    All tasks of the lesson are done

--- Lesson1.py
+++ Lesson1.py
@@ -1,1 +1,10 @@
-print("this world is to small for both of us")
+'''Task 2'''
+#Just a boring print function
+print("this world is to small for both of us\n")
+#Print with end parameter
+print("can I get a HOOYYYAAAA", end="!?!\n\n")
+#print with separator
+print(1,2,3,4,5,"\n", sep=" | ")
+'''Task 3'''
+print("#########","\n#\t#"*3,"\n#########\n")
+print("\n#\t#\n#\t#\n#########\n#\t#\n#\t#\n")
